a03da0f8d2503136f2ee5ee589460371d8ff0837
kang <java7ang@gmail.com>
2020-06-05 20:58:33 +0900

    myshop 코드 단방향 관계로 코드 수정 * 단방향 관계로 코드 수정

--- jpaStudy/ch1realex/src/main/java/mybook/myshop/domain/Order.java
+++ jpaStudy/ch1realex/src/main/java/mybook/myshop/domain/Order.java
@@ -12,12 +12,58 @@ public class Order {
 	private Long id;
 
 	//이러한 설계는 관계형 db 에 객체를 맞춘 설계임
-	@Column(name = "MEMBER_ID")
-	private Long memberId;
+	//@Column(name = "MEMBER_ID")
+	//private Long memberId;
+
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
 
 	private LocalDateTime orderDate;
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
 
+	public Order() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	//	public Long getMemberId() {
+//		return memberId;
+//	}
+
+//	public void setMemberId(Long memberId) {
+//		this.memberId = memberId;
+//	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 }
--- jpaStudy/ch1realex/src/main/java/mybook/myshop/domain/OrderItem.java
+++ jpaStudy/ch1realex/src/main/java/mybook/myshop/domain/OrderItem.java
@@ -1,9 +1,6 @@
 package mybook.myshop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class OrderItem {
@@ -12,11 +9,19 @@ public class OrderItem {
 	@Column(name = "ORDER_ITEM_ID")
 	private Long id;
 
-	@Column(name = "ORDER_ID")
-	private Long orderId;
+//	@Column(name = "ORDER_ID")
+//	private Long orderId;
 
-	@Column(name = "ITEM_ID")
-	private Long itemId;
+	@ManyToOne
+	@JoinColumn(name = "ORDER_ID")
+	private Order order;
+
+//	@Column(name = "ITEM_ID")
+//	private Long itemId;
+
+	@ManyToOne
+	@JoinColumn(name = "ITEM_ID")
+	private Item item;
 
 	private int orderPrice;
 	private int count;
@@ -24,10 +29,19 @@ public class OrderItem {
 	public OrderItem() {
 	}
 
-	public OrderItem(Long id, Long orderId, Long itemId, int orderPrice, int count) {
+//	public OrderItem(Long id, Long orderId, Long itemId, int orderPrice, int count) {
+//		this.id = id;
+//		this.orderId = orderId;
+//		this.itemId = itemId;
+//		this.orderPrice = orderPrice;
+//		this.count = count;
+//	}
+
+
+	public OrderItem(Long id, Order order, Item item, int orderPrice, int count) {
 		this.id = id;
-		this.orderId = orderId;
-		this.itemId = itemId;
+		this.order = order;
+		this.item = item;
 		this.orderPrice = orderPrice;
 		this.count = count;
 	}
@@ -40,20 +54,36 @@ public class OrderItem {
 		this.id = id;
 	}
 
-	public Long getOrderId() {
-		return orderId;
+//	public Long getOrderId() {
+//		return orderId;
+//	}
+//
+//	public void setOrderId(Long orderId) {
+//		this.orderId = orderId;
+//	}
+//
+//	public Long getItemId() {
+//		return itemId;
+//	}
+//
+//	public void setItemId(Long itemId) {
+//		this.itemId = itemId;
+//	}
+
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
-	public Long getItemId() {
-		return itemId;
+	public Item getItem() {
+		return item;
 	}
 
-	public void setItemId(Long itemId) {
-		this.itemId = itemId;
+	public void setItem(Item item) {
+		this.item = item;
 	}
 
 	public int getOrderPrice() {
