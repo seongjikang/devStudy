198c603728c8442a6066a727cbf40e4c130de3e0
Kang Seongji <java7ang@naver.com>
2020-06-09 14:54:20 +0900

    MappedSuperclass 추가 * 상속과는 다르게 속성만 가져옴 . * 이걸해주면 각테이블에 공통 속성을 넣어줄 수 있는 이점이 있다.

--- jpaStudy/ch2relatedmapping/src/main/java/test/myteam/domain/Item.java
+++ jpaStudy/ch2relatedmapping/src/main/java/test/myteam/domain/Item.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) // JOINED : 기본적으로 정석이라고 보면될듯, SINGLE_TABLE : 심플하게 조회됨, TABLE_PER_CLASS: 조회시 ... union으로 다 뒤지는 경우가 발생할 수 있어 비효율적임 ..
 @DiscriminatorColumn //이거 넣어 주면 자식 Entity 명이 DType이라는 컬럼으로 들어옴 , 자식 객체에서 DiscriminatorValue 로 Entity 명 말고 받고싶은걸로 변경가능, 단일테이블전략에서는 이거 안해줘도 자동으로 생김
-public abstract class Item {
+public abstract class Item extends BaseEntity{
     @Id
     @GeneratedValue
     private Long id;
