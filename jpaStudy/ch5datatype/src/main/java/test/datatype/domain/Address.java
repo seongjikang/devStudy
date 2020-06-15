a94629a9db6e60f0dae5c44e387afda5898bc298
kang <java7ang@gmail.com>
2020-06-16 00:17:24 +0900

    값 타입의 비교
    * primitive 타입을 제외한 비교는 equals로 할것
    * override 시킨 메서드를 만들어서 비교할 것
    * hashCode는 equals 메서드 오버라이드 시킬 때 한세트이다.

--- jpaStudy/ch5datatype/src/main/java/test/datatype/domain/Address.java
+++ jpaStudy/ch5datatype/src/main/java/test/datatype/domain/Address.java
@@ -2,6 +2,7 @@ package test.datatype.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -47,4 +48,22 @@ public class Address {
 //	public void setZipcode(String zipcode) {
 //		this.zipcode = zipcode;
 //	}
+
+	// 기본적으로 값타입을 비교하기 위한 equals (동등성 비교) 오버라이드 메서드.
+	// 동일성 비교는 == 으로 !! (primitive 타입은 이걸로.. 그외는 equals 로 !!)
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Address address = (Address) o;
+		return Objects.equals(city, address.city) &&
+				Objects.equals(street, address.street) &&
+				Objects.equals(zipcode, address.zipcode);
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, street, zipcode);
+	}
 }
