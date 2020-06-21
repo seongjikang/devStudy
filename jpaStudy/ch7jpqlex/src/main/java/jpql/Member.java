a28ff2c784f63accb226ccc1aceca6210d620405
kang <java7ang@gmail.com>
2020-06-21 10:52:00 +0900

    JPA JOIN
    * JPQL JOIN 관련 학습

--- jpaStudy/ch7jpqlex/src/main/java/jpql/Member.java
+++ jpaStudy/ch7jpqlex/src/main/java/jpql/Member.java
@@ -11,9 +11,15 @@ public class Member {
 	private String userName;
 	private int age;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY) //중요 !
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
+
+	//연관관계 편의 메서드
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
+	}
 
 	public Long getId() {
 		return id;
@@ -37,6 +43,14 @@ public class Member {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
 	}
 
 	@Override
