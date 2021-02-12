d30601fa7f49a5d82c931ed1577c06bf4207eaf7
kang <java7ang@gmail.com>
2021-02-12 14:30:39 +0900

    회원 레퍼지토리 개발 마무리
    * 회원 레퍼지토리 까지 개발을 마무리
    * 설명이 필요한 곳에 주석을 조금더 추가함
    
    ref #50

--- springjpaStudy/myshop/src/main/java/mybook/myshop/repository/MemberRepository.java
+++ springjpaStudy/myshop/src/main/java/mybook/myshop/repository/MemberRepository.java
@@ -7,10 +7,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+// Component Scan 으로 인해 자동으로 등록됨
 //스프링 빈으로 등록
 @Repository
 public class MemberRepository {
 
+    //얘는 Spring 이 엔티티 매니저를 주입해줌
+    // 예전에 팩토리 만들어서 엔티티 매니저 막 직접 꺼내서 하던 과정을 안해도됨
     @PersistenceContext
     private EntityManager em;
 
@@ -24,6 +27,7 @@ public class MemberRepository {
 
     public List<Member> findAll() {
         // 엔티티 객체를 대상을 쿼리를 날림 ..!
+        //sql 이랑 거의 비슷하지만 차이는 sql은 테이블을, jpql은 엔티티를 조회함
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
