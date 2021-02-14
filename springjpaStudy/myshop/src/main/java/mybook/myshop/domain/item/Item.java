42e5fc4321cc4504a286555661991c0424360e15
seongjikang <java7ang@gmail.com>
2021-02-14 14:12:29 +0900

    상품 도메인 개발하기
    * 상품 Entity에 비지니스 로직 추가함 (재고 증가, 감소 로직 추가)
    * ItemRepository 개발
    * ItemService 개발
    
    ref #56

--- springjpaStudy/myshop/src/main/java/mybook/myshop/domain/item/Item.java
+++ springjpaStudy/myshop/src/main/java/mybook/myshop/domain/item/Item.java
@@ -3,6 +3,7 @@ package mybook.myshop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import mybook.myshop.domain.Category;
+import mybook.myshop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@Getter @Setter
+@Getter //@Setter
 public abstract class Item {
 
 	@Id
@@ -24,4 +25,20 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+
+	//setter를 쓰지 않고 필요에 따라 비지니스로직을 만들어 주는것 ..! 이게 바로 객체지향이지지
+	// 재고 수량 증가하는 비지니스 로직을 추가
+	public void addStock(int quantity) {
+		this.stockQuantity = quantity;
+	}
+
+	// 재고 수량 감소하는 비지니스 로직 추가
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 }
