5e956de1708977a8a31cabbae30e1aa81b1f6b98
seongjikang <java7ang@gmail.com>
2021-02-16 18:05:17 +0900

    회원 조회 화면 개발
    * memberList.html 개발하기
    * 지금은 entity를 리턴함
    * 실무에서는 dto를 리턴하도록
    
    ref #68

--- springjpaStudy/myshop/src/main/java/mybook/myshop/controller/MemberForm.java
+++ springjpaStudy/myshop/src/main/java/mybook/myshop/controller/MemberForm.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotEmpty;
 
 //Entity 와 Form 데이타는 구분해주는게 좋다 ~~!!
 // Entity 와 Form 이 하는 역할은 서로 다르다. (원하는 밸리데이션이 다르다.)
+// 화면 에 대한 내용은 Form 객체나 dto에 있어야함, Entity는 핵심 비지니스 로직만 있어야함
 @Getter @Setter
 public class MemberForm {
 
