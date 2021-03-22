9a23e641325eb08bf8c3cb4853189b90a5ef1735
seongjikang <java7ang@gmail.com>
2021-03-22 18:06:00 +0900

    CLI 에서 프로젝트 하는 방법
    * 싱글 파일 컴포넌트에서 공부한거 적용
    * 싱글 파일 컴포넌트 체계에서 컴포넌트 등록
    * 싱글 파일 컴포넌트에서 event emit 구현
    
    ref #102

--- vue-js/my-vue-pjt/src/App.vue
+++ vue-js/my-vue-pjt/src/App.vue
@@ -1,28 +1,41 @@
 <template>
+  <!-- template div는 하나만 있어야함 (root 니깐) -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <app-header v-vind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr">
+     </app-header>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
 
 export default {
-  name: 'App',
+  // 아래는 ... 이제 여기서만 쓸 객체를 리턴해주느 걸로
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
