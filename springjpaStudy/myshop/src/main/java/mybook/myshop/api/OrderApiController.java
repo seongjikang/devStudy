8f11d07943dd7355cc76f185bf46f82dcfeceecc
강성지 <java7ang@naver.com>
2021-05-04 23:17:57 +0900

    컬렉션 조회 2
    * dto로조회
    * 내부에 조회되는 모든것을 ... dto로 바꾼다는 생각을 하는게 좋음
    
    ref #124

--- springjpaStudy/myshop/src/main/java/mybook/myshop/api/OrderApiController.java
+++ springjpaStudy/myshop/src/main/java/mybook/myshop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package mybook.myshop.api;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import mybook.myshop.domain.Address;
 import mybook.myshop.domain.Order;
 import mybook.myshop.domain.OrderItem;
+import mybook.myshop.domain.OrderStatus;
 import mybook.myshop.repository.OrderRepository;
 import mybook.myshop.repository.OrderSearch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +31,57 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        //엔티티가 외부에 노출되는 심각한 문제가 있는 코드임....
+        //private List<OrderItem> orderItems;
+
+        // 이렇게 바꾸주자..
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            // 초기에 프록시 초기화하고 돌려야함.
+            //order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+            //orderItems = order.getOrderItems();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int orderCount;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            orderCount = orderItem.getCount();
+        }
     }
 }
