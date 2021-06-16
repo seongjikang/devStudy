65f092c7c3bef3c1adb6720c77b03df09012b0db
seongjikang <java7ang@gmail.com>
2021-06-16 18:26:36 +0900

    Data JPA 공통 인터페이스
    * 공통 인터페이스 설정
    * 공통 인터페이스 적용
    * 공통 인터페이스 분석
    
    ref #155

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,9 +1,10 @@
-package study.datajpa;
+package study.datajpa; //이 패키지 하위는 전부다 끌어올수 있음
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication //@EnableJpaRepositories(basePackages = "study.datajpa.repository") // Spring Boot 에서는 필요없는 어노테이션
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
