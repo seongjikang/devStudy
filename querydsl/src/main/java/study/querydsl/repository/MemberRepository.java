9bb3678b7110a8b4ff3297c91e897f77f187a0bc
seongjikang <java7ang@gmail.com>
2021-07-10 02:19:13 +0900

    커스텀한 QueryDslRepositorySupport 만들기

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
